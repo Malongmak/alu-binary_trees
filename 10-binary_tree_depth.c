499ad8a0dfc5b7792417bd475c0d371a3a7c0732
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-11 14:23:52 -0300

    10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,19 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the height of a binary tree
+ * binary_tree_depth - measures the depth of a node in binary tree
  * @tree: binary tree
- * Return: height or 0 if tree is null
+ * Return: depth or 0 if tree is null
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-size_t left, right;
-if (!tree || (!tree->right && !tree->left))
+size_t depth;
+if (!tree || !tree->parent)
 {
 return (0);
 }
-left = binary_tree_depth(tree->left);
-right = binary_tree_depth(tree->right);
-return ((left > right ? left : right)+1);
+depth = binary_tree_depth(tree->parent);
+return (depth+1);
 }
