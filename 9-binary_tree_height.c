b2c3cfb9cafb65a48fee0093e05713ff4de397b0
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-11 13:54:35 -0300

    9-binary_tree_height.c

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,22 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: binary tree
+ * Return: height or 0 if tree is null
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t height = 0;
+if (!tree)
+{
+return (height);
+}
+while (tree->left)
+{
+height++;
+tree = tree->left;
+}
+return (height);
+}
