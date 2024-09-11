3a39e9bcd051aecbde51ce7f3525b623699029df
Malongmak <j.malong@alustudent.com>
2024-09-11 05:01:24 -0700

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,108 +1,14 @@
-/* binary_tree.h */
-#ifndef BINARY_TREE_H
-#define BINARY_TREE_H
-
-#include <stddef.h>
+#include "binary_trees.h"
 
 /**
- * struct binary_tree_s - Binary tree node
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: node to check
  *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
+ * Return: 1 if node is a leaf
+ *         0 if not a leaf
+ *         0 if node is NULL
  */
-struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-
-/* Function prototypes */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-int binary_tree_is_leaf(const binary_tree_t *node);
-void binary_tree_print(const binary_tree_t *tree);
-
-#endif /* BINARY_TREE_H */
-
-/* 0-binary_tree_node.c */
-#include <stdlib.h>
-#include "binary_tree.h"
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return NULL;
-
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    return new_node;
-}
-
-/* 2-binary_tree_insert_right.c */
-#include "binary_tree.h"
-
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-    if (parent == NULL)
-        return NULL;
-
-    binary_tree_t *new_node = binary_tree_node(parent, value);
-    if (new_node == NULL)
-        return NULL;
-
-    if (parent->right != NULL)
-    {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-    }
-    parent->right = new_node;
-
-    return new_node;
-}
-
-/* 4-binary_tree_is_leaf.c */
-#include "binary_tree.h"
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return 0;
-
-    return (node->left == NULL && node->right == NULL);
-}
-
-/* 4-main.c */
-#include <stdlib.h>
-#include <stdio.h>
-#include "binary_tree.h"
-
-int main(void)
-{
-    binary_tree_t *root;
-    int ret;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
-
-    ret = binary_tree_is_leaf(root);
-    printf("Is %d a leaf: %d\n", root->n, ret);
-    ret = binary_tree_is_leaf(root->right);
-    printf("Is %d a leaf: %d\n", root->right->n, ret);
-    ret = binary_tree_is_leaf(root->right->right);
-    printf("Is %d a leaf: %d\n", root->right->right->n, ret);
-    return (0);
+	return ((!node || node->left || node->right) ? 0 : 1);
 }
