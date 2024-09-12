2fb52b4c14e724fbdef2642ea35113e8f1f92f16
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-12 08:33:57 -0300

    added height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,22 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: binary tree
+ * Return: height or 0 if tree is null
+ */
+
+int binary_tree_height(const binary_tree_t *tree)
+{
+int left, right;
+if (!tree || (!tree->right && !tree->left))
+{
+return (0);
+}
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+return ((left > right ? left : right)+1);
+}
+
 
 /**
  * binary_tree_balance - measures the balance factor of binary tree
