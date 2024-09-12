ade3d37a06043b393ddddd98000f2cf7d335a733
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-12 08:23:02 -0300

    14-binary_tree_balance.c 2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,7 @@
  * Return: 0 if tree is null else balance factor
  */
 
-int binary_tree_balance(const binary_tree *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 int left, right;
 if (!tree)
