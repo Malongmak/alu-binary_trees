40dd33b885daa3eb417bcf215769cd23b9188595
dkokonkwo <d.okonkwo@alustudent.com>
2024-09-12 08:40:46 -0300

    linker

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,21 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: binary tree
+ * Return: height or 0 if tree is null
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left, right;
+if (!tree || (!tree->right && !tree->left))
+{
+return (0);
+}
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+return ((left > right ? left : right)+1);
+}
 
 /**
  * binary_tree_balance - measures the balance factor of binary tree
